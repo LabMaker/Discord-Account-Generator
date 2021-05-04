7ced6e6008df0cbcb90289db6d6ac0d42062a226
DanyaalA <Danny13653@gmail.com>
2021-05-04 13:39:29 +0100

    Added Verification & Captcha

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import puppeteer from "puppeteer-extra";
 import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(
-  // RecaptchaPlugin({
-  //   provider: { id: "2captcha", token: "42bdee5c3b9b711ec70d4f8b886a88c3" },
-  //   visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
-  // }),
+  RecaptchaPlugin({
+    provider: { id: "2captcha", token: "42bdee5c3b9b711ec70d4f8b886a88c3" },
+    visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
+  }),
   StealthPlugin()
 );
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    //args: ["--proxy-server=http://45.136.231.64:7120"],
+    args: ["--proxy-server=http://37.35.41.169:8515"],
   });
   const context = await browser.createIncognitoBrowserContext();
 
@@ -20,6 +20,7 @@ puppeteer.use(
 
   const emailPage = await context.newPage();
   const discordPage = await context.newPage();
+
   console.log("/.///..");
 
   await emailPage.goto("https://mail.tm/en/", {
@@ -66,45 +67,98 @@ puppeteer.use(
   await discordPage.keyboard.type("12");
   await discordPage.keyboard.press("Tab");
   await discordPage.keyboard.type("1995");
-  await discordPage.click(checkBoxSelector);
+  try {
+    await discordPage.click(checkBoxSelector);
+  } catch {
+    console.log("ChecBox Doesnt Exist");
+  }
   await discordPage.click(submitbutton);
 
-  // setTimeout(async () => {
-  //   await discordPage.solveRecaptchas();
-  //   await Promise.all([discordPage.waitForNavigation()]);
-  // }, 5000);
+  setTimeout(async () => {
+    await discordPage.solveRecaptchas();
+    await Promise.all([discordPage.waitForNavigation()]);
+    emailPage.bringToFront();
+  }, 5000);
 
-  //await discordPage.type(".css-1hwfws3", "12");
-
+  let done = false;
+  let mailID = "";
   // Wait until email is recieved and click on it.
   const emailResponseHandler = async (r) => {
+    if (emailPage.url.toString().includes("mail.tm/en/view/")) {
+      console.log("Hit First return");
+      return;
+    }
+
+    const linkHandlers = await emailPage.$x("//a[contains(text(), 'Refresh')]");
+    if (linkHandlers.length > 0) {
+      await linkHandlers[0].click();
+    } else {
+      console.log("Link Not Available");
+    }
+
     if (
       r.request().method() == "GET" &&
-      r.request().url().includes("https://web1.temp-mail.org/request/mails/id/")
+      r.request().url().includes("https://api.mail.tm/messages")
     ) {
       let body = JSON.parse(await r.text());
+
+      if (body == undefined) {
+        return;
+      }
 
       // 'no emails yet' msg is in .error, so when it is undefined, we have an email.
       // When we have an email, navigate to it.
       if (body.error === undefined) {
-        let mailID = body[0].mail_id;
-        emailPage.goto(`https://temp-mail.org/en/view/${mailID}`);
+        mailID = body["hydra:member"][0].id;
+        console.log(mailID);
+        emailPage.goto(`https://mail.tm/en/view/${mailID}/`);
+        done = true;
         emailPage.off("response", emailResponseHandler);
+        //emailPage.goto(`https://temp-mail.org/en/view/${mailID}`);
+      } else {
+        return;
       }
 
-      setInterval(async () => {
-        const linkHandlers = await emailPage.$x(
-          "//a[contains(text(), 'Verify')]"
+      setTimeout(async () => {
+        await emailPage.waitForSelector("iframe");
+        const elementHandle = await emailPage.$("div.h-full iframe");
+        const frame = await elementHandle.contentFrame();
+
+        const linkHandlers = await frame.$x(
+          "//a[contains(text(), 'Verify Email')]"
         );
+        // const linkHandlers = await emailPage.$x(
+        //   "//a[contains(text(), 'Verify Email')]"
+        // );
         if (linkHandlers.length > 0) {
-          await linkHandlers[0].click();
+          //await linkHandlers[0].click();
+          const verifyUrlPromise = await linkHandlers[0].getProperty("href");
+          let verifyUrl = "";
+          verifyUrlPromise.jsonValue().then((res) => {
+            console.log(res);
+            verifyUrl = res;
+          });
+
+          const veriifyPage = await context.newPage();
+          veriifyPage.goto(verifyUrl);
+
+          setTimeout(async () => {
+            await veriifyPage.solveRecaptchas();
+            await Promise.all([veriifyPage.waitForNavigation()]);
+          }, 5000);
+
           emailPage.off("response", emailResponseHandler);
         } else {
           console.log("Link Not Available");
         }
-      }, 4000);
+      }, 10000);
     }
   };
-  emailPage.on("response", emailResponseHandler);
+  if (!done) {
+    emailPage.on("response", emailResponseHandler);
+  } else {
+    emailPage.off("response", emailResponseHandler);
+  }
+
   // await browser.close();
 })();
